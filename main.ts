0d31b699306bd89185debcb6ca92dd7c97f5b2cf
Bewis09 <bewis09minecraft@gmail.com>
2025-06-22 14:41:03 +0200

    Refactor zip functionality and add HTML rules page with server-side integration

--- main.ts
+++ main.ts
@@ -1,28 +1,54 @@
-import {
-    BlobWriter,
-    ZipWriter,
-} from "https://deno.land/x/zipjs@v2.7.54/index.js";
+import { BlobWriter, ZipWriter } from "zip";
 
-const zipFileWriter = new BlobWriter();
+async function toZip() {
+    const zipFileWriter = new BlobWriter();
 
-const zipWriter = new ZipWriter(zipFileWriter);
+    const zipWriter = new ZipWriter(zipFileWriter);
 
-function recursivePaths(path: string) {
-    for (const dirEntry of Deno.readDirSync("src/" + path)) {
-        if(dirEntry.isDirectory)
-            recursivePaths(path=="" ? dirEntry.name : path + "/" + dirEntry.name)
-        else
-            zipWriter.add(path=="" ? dirEntry.name : path + "/" + dirEntry.name, Deno.openSync("src/" + path + "/" + dirEntry.name))
+    function recursivePaths(path: string) {
+        for (const dirEntry of Deno.readDirSync("src/" + path)) {
+            if (dirEntry.isDirectory) {
+                recursivePaths(
+                    path == "" ? dirEntry.name : path + "/" + dirEntry.name,
+                );
+            } else {
+                zipWriter.add(
+                    path == "" ? dirEntry.name : path + "/" + dirEntry.name,
+                    Deno.openSync("src/" + path + "/" + dirEntry.name),
+                );
+            }
+        }
     }
+
+    recursivePaths("");
+    await zipWriter.close();
+
+    return await (await zipFileWriter.getData()).bytes();
 }
 
-recursivePaths("")
-await zipWriter.close();
+// deno-lint-ignore no-explicit-any
+let bytes: any | undefined = undefined;
 
-const bytes = await (await zipFileWriter.getData()).bytes()
+let html: string | undefined = undefined;
 
-Deno.serve(((req) => {
-    if(new URL(req.url).pathname == "/resource_pack.zip")
-        return new Response(bytes)
-    return new Response("Not Found", { status: 404, statusText: "Not Found" })
-}))
+Deno.serve(async (req) => {
+    if (new URL(req.url).pathname == "/resource_pack.zip") {
+        if (bytes === undefined) {
+            bytes = await toZip();
+        }
+
+        return new Response(bytes);
+    }
+    if (new URL(req.url).pathname.match(/^\/rules\/?$/)) {
+        if (html === undefined || true) {
+            html = Deno.readTextFileSync("index.html").replace("rule_server_insert_here", Deno.readTextFileSync("rules/rules.md"));
+        }
+
+        return new Response(html, {
+            headers: {
+                "Content-Type": "text/html; charset=utf-8",
+            },
+        });
+    }
+    return new Response("Not Found", { status: 404, statusText: "Not Found" });
+});
